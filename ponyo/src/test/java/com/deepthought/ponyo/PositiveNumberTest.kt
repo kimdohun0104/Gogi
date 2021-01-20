1c6496311f80c6cd4372715f91bae397ecb950aa
kimdohun <kimdohun0104@gmail.com>
2021-01-21 00:26:42 +0900

    테스트 케이스 추가

--- ponyo/src/test/java/com/deepthought/ponyo/PositiveNumberTest.kt
+++ ponyo/src/test/java/com/deepthought/ponyo/PositiveNumberTest.kt
@@ -11,6 +11,12 @@ class PositiveNumberTest {
     fun `when value is negative`() {
         val positiveNumber = PositiveNumber.of(-1)
         assertTrue(positiveNumber is Constrained.Invalid)
-        println((positiveNumber as Constrained.Invalid).message)
+        println((positiveNumber as? Constrained.Invalid)?.message)
+    }
+
+    @Test
+    fun `when value is positive`() {
+        val positiveNumber = PositiveNumber.of(1)
+        assertTrue(positiveNumber is Constrained.Valid)
     }
 }
